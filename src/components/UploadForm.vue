fa42a95fc77d99b91bd5a24a70c938423230a5c0
Warren1012 <annwar101@gmail.com>
2022-04-10 18:58:56 -0500

    Excerise 3 error

---
+++ src/components/UploadForm.vue
@@ -0,0 +1,19 @@
+<template>
+<form id="upload" name="upload" method="POST" enctype="multipart/form-data" @submit.prevent="uploadPhoto">
+    <div class="form-group">
+        {{ form.description.label }}
+        {{ form.description(class='form-control') }}
+    </div>
+
+    <div class="form-group">
+        {{ form.image.label }}
+        {{ form.image(class="form-control")}}
+    </div>
+    <button type="submit" name="submit" class="btn">Sumbit</button>
+</form>
+<p>HI </p>
+</template>
+
+<script>
+    import UploadForm from app.forms
+</script>
